6877401cf1c28d3641150ab05482d52b92010a0e
Zegendary <zxw1992513@qq.com>
2019-03-12 16:57:35 +0800

    redux-demo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {inputValue: 0}
+  }
+
+  addIfOdd = ()=>{
+    if(this.props.value % 2 === 0){
+      this.props.onChange({type: 'INCREMENT'}) 
+    }
+  }
+
+  addAsync = () => {
+    setTimeout(()=>{
+      this.props.onChange({type: 'INCREMENT'})
+    },2000)
+  }
+
+  add2 = () =>{
+    this.props.onChange({type: 'ADD_2'})
+  }
+
+  addAny = () =>{
+    this.props.onChange({type: 'ADD_ANY',payload: +this.state.inputValue})
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <p>
+          Clicked: <span id="value">{this.props.value}</span> times
+          {/* <button onClick={()=>this.props.onChange({type: 'INCREMENT'})}>+</button>
+          <button onClick={()=>this.props.onChange({type: 'DECREMENT'})}>-</button>
+          <button onClick={this.addIfOdd}>如果是双数就+1</button>
+          <button onClick={this.addAsync}>异步+1</button>
+          <button onClick={this.add2}>+2</button> */}
+          <input value={this.state.inputValue} onChange={(e)=> this.setState({inputValue: e.target.value})}/>
+          <button onClick={this.addAny}>添加</button>
+        </p>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,10 +1,45 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import * as Redux from 'redux'
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const reducer = (state,action)=>{
+ if(state === undefined) {
+  state = 0
+ }
+ switch(action.type){
+  case 'INCREMENT':
+    return state + 1;
+  case 'DECREMENT':
+    return state -1;
+  case 'ADD_2':
+    return state + 2;
+  case 'ADD_ANY':
+    return state + action.payload;
+  default:
+    return state;
+ }
+}
+
+const store = Redux.createStore(reducer)
+
+const change = (action) => {
+  store.dispatch(action)
+}
+
+// const minus = () => {
+//   store.dispatch({type: 'DECREMENT'})
+// }
+
+const render = ()=>{
+  ReactDOM.render(<App value={store.getState()} onChange={change}/>, document.getElementById('root'));
+}
+render()
+store.subscribe(render)
+
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
